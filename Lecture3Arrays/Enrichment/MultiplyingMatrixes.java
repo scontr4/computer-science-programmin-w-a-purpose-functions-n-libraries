8ade0876deb0b406cf90c3bc5d787c3eb940acfc
SebastianC <sebastian.contreras@btgpactual.com>
2023-10-19 18:32:09 -0500

    Initialized the folders of module 3

---
+++ Lecture3Arrays/Enrichment/MultiplyingMatrixes.java
@@ -0,0 +1,14 @@
+public class MultiplyingMatrixes {
+    
+    public static void main(String[] args) {
+
+        int rows_a = Integer.parseInt(args[0]);
+        int cols_a = Integer.parseInt(args[1]);
+
+        int rows_b = Integer.parseInt(args[2]);
+        int cols_b = Integer.parseInt(args[3]);
+
+        // Multiplying a matrix a by matrix b is equal to 
+    }
+
+}
