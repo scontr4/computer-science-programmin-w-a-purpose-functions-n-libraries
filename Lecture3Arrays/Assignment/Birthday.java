622618dc3f9d3dac33d4c180cedd7275ab2ccb73
SebastianC <sebastian.contreras@btgpactual.com>
2023-10-26 11:52:07 -0500

    Birthday looks good, it gets the idea but now I have to print for each number of entries

--- Lecture3Arrays/Assignment/Birthday.java
+++ Lecture3Arrays/Assignment/Birthday.java
@@ -1,8 +1,29 @@
+import java.lang.Math;
+
 public class Birthday {
 
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int trials = Integer.parseInt(args[0]);
+
+        for (int trial = 0; trial < trials; trial++) {
+            
+            int count = 0;
+            boolean[] birthdays = new boolean[n];
+
+            while (true) {
+                int newBirthday = (int) (Math.random() * n);
+
+                if (birthdays[newBirthday] == false) {
+                    count++;
+                    birthdays[newBirthday] = true;
+                } else {
+                    break;
+                }
+            }
+
+        }
+
     }
 
 }
