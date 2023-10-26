ad68d1b903f890647434bcf161135a9a711ed5af
SebastianC <sebastian.contreras@btgpactual.com>
2023-10-26 11:16:46 -0500

    Thue Morse wave done in assignment lecture 3

--- Lecture3Arrays/Assignment/ThueMorse.java
+++ Lecture3Arrays/Assignment/ThueMorse.java
@@ -5,6 +5,7 @@ public class ThueMorse {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         
+        // Build the Thue-Morse sequence
         int[] thueMorseSequence = new int[n];
         System.out.print(thueMorseSequence[0] + " ");
         for (int i=1; i < n; i++) {
@@ -19,7 +20,20 @@ public class ThueMorse {
                 thueMorseSequence[i] = 0;
             }
             System.out.print(thueMorseSequence[i] + " ");
+        }
+        System.out.println();
 
+        // Create the Thue-Morse wave
+        for (int i = 0; i < n; i++) {    
+            for (int j = 0; j < n; j++) {
+                if (thueMorseSequence[i] == thueMorseSequence[j]) {
+                    System.out.print("+  ");
+                } 
+                else {
+                    System.out.print("-  ");
+                }
+            }
+            System.out.println();
         }
     }
 }
