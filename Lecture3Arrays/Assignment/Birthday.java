bb1e1af14ee477169cc01501ce03bf68a3f57bde
SebastianC <sebastian.contreras@btgpactual.com>
2023-10-26 12:14:33 -0500

    I have the counter, now how do I get the times each one happens in an loop that finalized when I reach 50%?

--- Lecture3Arrays/Assignment/Birthday.java
+++ Lecture3Arrays/Assignment/Birthday.java
@@ -4,23 +4,27 @@ public class Birthday {
 
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        int trials = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
 
-        for (int trial = 0; trial < trials; trial++) {
+        for (int trial = 0; trial < trials; trial++) {            
             
-            int count = 0;
-            boolean[] birthdays = new boolean[n];
+            int count = 1;
 
+            // Entries until 2 people match their birthday
+            boolean[] birthdays = new boolean[n];            
             while (true) {
-                int newBirthday = (int) (Math.random() * n);
+                int newBirthday = (int) (Math.random() * n);                
 
                 if (birthdays[newBirthday] == false) {
-                    count++;
                     birthdays[newBirthday] = true;
                 } else {
                     break;
                 }
+
+                count++;
             }
+
+            System.out.println(count);
 
         }
 
