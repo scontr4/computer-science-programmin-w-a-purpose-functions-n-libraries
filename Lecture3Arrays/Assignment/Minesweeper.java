673ca4dfc29ecc401e1d8866bc433d4a4116a25f
SebastianC <sebastian.contreras@btgpactual.com>
2023-10-27 10:18:04 -0500

    Minesweeper done

--- Lecture3Arrays/Assignment/Minesweeper.java
+++ Lecture3Arrays/Assignment/Minesweeper.java
@@ -5,7 +5,7 @@ public class Minesweeper {
         int n = Integer.parseInt(args[1]);
         int k = Integer.parseInt(args[2]);
 
-        // two extra rows so i don't get out of bounds errors
+        // Two extra rows so i don't get out of bounds errors
         // I will truncate them later
         int[][] grid = new int[m + 2][n + 2]; 
         for(int i=0; i < k; i++) {
@@ -17,38 +17,19 @@ public class Minesweeper {
                 if (grid[randRow][randCol] != -1) {
                     grid[randRow][randCol] = -1;
 
-                    if(grid[randRow][randCol + 1] != -1) {
-                        grid[randRow][randCol + 1]++;
-                    }
-                    if(grid[randRow][randCol - 1] != -1) {
-                        grid[randRow][randCol - 1]++;                        
-                    }
-
-                    if(grid[randRow + 1][randCol] != -1) {
-                        grid[randRow + 1][randCol]++;
-                    }
-                    if(grid[randRow - 1][randCol] != -1) {
-                        grid[randRow - 1][randCol]++;
-                    }
-
-                    if(grid[randRow + 1][randCol + 1] != -1) {
-                        grid[randRow + 1][randCol + 1]++;
-                    }
-                    if(grid[randRow + 1][randCol - 1] != -1) {
-                        grid[randRow + 1][randCol - 1]++;
-                    }
-                    if(grid[randRow - 1][randCol - 1] != -1) {
-                        grid[randRow - 1][randCol - 1]++;
-                    }
-                    if(grid[randRow - 1][randCol + 1] != -1) {
-                        grid[randRow - 1][randCol + 1]++;
-                    }                
+                    for (int x = -1; x <= 1; x++) {
+                        for (int y = -1; y <= 1; y++) {
+                            if (grid[randRow + x][randCol + y] != -1) {
+                                grid[randRow + x][randCol + y]++;
+                            }
+                        }
+                    }          
                     break;
                 }
             }
         }
 
-        
+        // Print the grid
         for (int i=1; i < m+1; i++){
             for (int j=1; j < m+1; j++){
                 if (grid[i][j] == -1) {
